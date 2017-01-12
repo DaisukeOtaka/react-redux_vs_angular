250e4c89df37477e125ce62e2c26cfede80d5e1e
daisuke.otaka <daisuke.otaka@globalway.co.jp>
2017-01-12 14:42:29 +0900

    Add heroku settings

--- index.js
+++ index.js
@@ -3,18 +3,19 @@
 //====================================================================================================
 // Libralies
 //====================================================================================================
-var express = require('express')
-var path = require('path')
+var express = require('express');
+var path = require('path');
 
 //====================================================================================================
 // Configuration
 //====================================================================================================
-var app = new express()
+var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 //====================================================================================================
 // Served Contents
 //====================================================================================================
+app.use('/', express.static(path.join(__dirname, 'angular1/dist')));
 app.use('/angular1', express.static(path.join(__dirname, 'angular1/dist')));
 app.use('/angular2', express.static(path.join(__dirname, 'angular2/dist')));
 app.use('/react-redux', express.static(path.join(__dirname, 'react-redux/dist')));
@@ -24,4 +25,4 @@ app.use('/react-redux', express.static(path.join(__dirname, 'react-redux/dist'))
 //====================================================================================================
 app.listen(app.get('port'), function() {
   console.log('Served: http://localhost:' + app.get('port'));
-})
+});
